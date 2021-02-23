b0138ab9ea7b7265cb19788609164a89c0997e68
Ilya Andreev <ilyaa1982@gmail.com>
2021-02-22 21:02:17 -0500

    mysql has been added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const pageNotFound = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -11,16 +12,12 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const addItemRoute = require('./routes/item');
-// const addCartRoute = require('./routes/cart');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(addItemRoute);
-// app.use(addCartRoute);
 
 app.use(pageNotFound.notFound);
-
-
 
 app.listen(3000);
